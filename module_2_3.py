47fb316022daaafcd93ea85f196add87e5e1b2d9
VetrAL25 <vetranapa@gmail.com>
2024-10-02 22:48:17 +0300

    module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,6 +1,6 @@
 my_list =  [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 nomer = 0
-while len (my_list)>nomer:
+while len (my_list) > nomer:
     if my_list [nomer] > 0:
         print(my_list [nomer])
         nomer += 1
@@ -9,5 +9,7 @@ while len (my_list)>nomer:
         if my_list[nomer] == 0:
          nomer += 1
         continue
+    if nomer > my_list [nomer]:
+        break
     if my_list[nomer] < 0:
         break
